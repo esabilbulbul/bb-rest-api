556a74e44a956b62918cc6c38d8bf81ee6252ef5
esabil <esabil@192.168.1.3>
2019-12-06 19:39:41 +0300

    merge conflict resolved (post battery replacement)

--- src/java/misc/DekontMisc.java
+++ src/java/misc/DekontMisc.java
@@ -5,6 +5,11 @@
  */
 package misc;
 
+import java.util.List;
+import jaxesa.persistence.EntityManager;
+import jaxesa.persistence.Query;
+import jaxesa.persistence.StoredProcedureQuery;
+import jaxesa.persistence.misc.RowColumn;
 import jaxesa.util.Util;
 
 /**
@@ -19,6 +24,59 @@ public final class DekontMisc
 
         return Long.toString(lDateTime);
     }
+
+    //This returns the name of file by the id from FTP
+    //psFileName = Id
+    public static String getFilePathbyId(String psFileName, String psRootPath, String psUserId)
+    {
+        //Root folder + userId + YYYYMMDD
+        String sYYYYMMDD   = psFileName.substring(0,8);
+        String sFilePath = psRootPath + "/" + psUserId + "/" + sYYYYMMDD + "/" + psFileName;
+
+        return sFilePath;
+    }
+
+    public static boolean isRecordAdded(EntityManager pem, String pRefNo, int pBankCode)
+    {
+        try
+        {
+            String sQuery = "SELECT COUNT(1) as CNT FROM ss_mrc_data_eod WHERE BANK_CODE = ? AND TXN_TRACE_NO = ? ";
+
+            //pem.gDriver
+
+            Query stmtQry = pem.CreateQuery(sQuery);
+
+            int ParIndex = 1;
+            stmtQry.SetParameter(ParIndex++, pBankCode, "P_BANK_CODE");
+            stmtQry.SetParameter(ParIndex++, pRefNo, "P_TXN_TRACE_NO");
+
+            List<List<RowColumn>> rs =  stmtQry.getResultList();
+            if (rs.size()==1)
+            {
+                List<RowColumn> RowN = rs.get(0);
+                
+                long lCnt =  Long.parseLong(Util.Database.getValString(RowN, "CNT"));
+                
+                if (lCnt>0)
+                    return true;
+                
+            }
+            //for (List<RowColumn> RowN:rs)
+            //{
+                //String sCNT = Util.Database.getValString(RowN, "CNT");
+                
+                
+            //}
+            
+            return false;
+        }
+        catch(Exception e)
+        {
+            return true;
+        }
+
+    }
+
 }
 
 
--- src/java/webapi/UI.java
+++ src/java/webapi/UI.java
@@ -7,9 +7,15 @@ package webapi;
 
 import bb.app.dekonts.DekontFields;
 import bb.app.dekonts.DekontMethods;
+import static bb.app.dekonts.DekontMisc.calculateSummaryBankSubtotals;
+import static bb.app.dekonts.DekontMisc.calculateSummaryDays;
+import static bb.app.dekonts.DekontMisc.calculateSummaryWeeksOfMonth;
 import bb.app.dekonts.DekontSummary;
-import bb.app.dekonts.MonthSummary;
-import entity.mrc.SsMrcPosReports;
+import entity.mrc.SsMrcDataEod;
+import entity.mrc.SsMrcDataEod;
+import entity.mrc.SsMrcDataPosTxn;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import jaxesa.annotations.Consumes;
 import jaxesa.annotations.GET;
@@ -21,6 +27,8 @@ import jaxesa.persistence.DBPool;
 import jaxesa.persistence.EntityManager;
 import jaxesa.util.Util;
 import jaxesa.webapi.sso_APIResponse;
+import misc.DekontMisc;
+import org.apache.pdfbox.pdmodel.PDDocument;
 
 /**
  *
@@ -30,10 +38,249 @@ import jaxesa.webapi.sso_APIResponse;
 public class UI 
 {
     
+    @GET
+    @Path("/api/getmonthstats/{userid},"
+                                + "{lang},"
+                                + "{country},"
+                                + "{sessionid}"
+                                + "{mrcid},"
+                                + "{year},"
+                                + "{month}"
+         )
+    @Consumes()
+    @Produces(MediaType.JSON)
+    public sso_APIResponse getMonthStats(  @PathParam("userid")                       String psUser_SessionInfo,
+                                            @PathParam("lang")                         String psLang,
+                                            @PathParam("country")                      String psCountry,
+                                            @PathParam("sessionid")                    String psSessionId,
+                                            @PathParam("mrcid")                        String psMrcId,
+                                            @PathParam("year")                         String psYear,
+                                            @PathParam("month")                        String psMonth
+                                         ) throws Exception
+    {
+        int iRec = 0;
+        try
+        {
+            sso_APIResponse Rsp = new sso_APIResponse();
+            String sTargetMonth = psMonth;
+            
+            DekontSummary summary = new DekontSummary();
+
+            EntityManager em = DBPool.getSessionConnection(psUser_SessionInfo, Util.Methods.hash());
+            
+            //Decide the year
+            // If the month is larger than this year's month then the year goes back as reference 
+            /*
+            int iTargetMonth = Integer.parseInt(sTargetMonth);
+            String sTargetYear = "";
+            String sCurrentYear  = Util.DateTime.GetDateTime_s().substring(0,4);
+            int iCurrentYear = Integer.parseInt(sCurrentYear);
+            String sCurrentMonth = Util.DateTime.GetDateTime_s().substring(4,6);
+            int iCurrentMonth = Integer.parseInt(sCurrentMonth);
+            
+            if(iTargetMonth>iCurrentMonth)
+            {
+                sTargetYear  = Integer.toString(iCurrentYear - 1);
+            }
+            else
+            {
+                sTargetYear = sCurrentYear;
+            }
+            */
+
+            String sCurrentYear  = Util.DateTime.GetDateTime_s().substring(0,4);
+            sTargetMonth = Util.Str.leftPad(sTargetMonth, "0", 2);
+            summary.currentMonth.days  = calculateSummaryDays(em, "-1", sTargetMonth);
+
+            summary.currentMonth.weeks = calculateSummaryWeeksOfMonth(em, "-1", sTargetMonth);
+            
+            summary.currentMonth.dayAvgs = bb.app.dekonts.DekontMisc.calculateSummaryTargetMonthDayAverages(em, sCurrentYear, sTargetMonth);
+
+            Rsp.Content = Util.JSON.Convert2JSON(summary).toString();
+            Rsp.Response = "ok";
+            return Rsp;
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+    }
+
+    @GET
+    @Path("/api/neweodentry/{userid},"
+                                + "{lang},"
+                                + "{country},"
+                                + "{sessionid}"
+                                + "{mrcid},"
+                                + "{year},"
+                                + "{month},"
+                                + "{day},"
+                                + "{tot}"
+         )
+    @Consumes()
+    @Produces(MediaType.JSON)
+    public sso_APIResponse addNewEndOfDay(  @PathParam("userid")                       String psUser_SessionInfo,
+                                            @PathParam("lang")                         String psLang,
+                                            @PathParam("country")                      String psCountry,
+                                            @PathParam("sessionid")                    String psSessionId,
+                                            @PathParam("mrcid")                        String psMrcId,
+                                            @PathParam("year")                         String psYear,
+                                            @PathParam("month")                        String psMonth,
+                                            @PathParam("day")                          String psDay,
+                                            @PathParam("tot")                          String psTot
+                                            ) throws Exception
+    {
+        int iRec = 0;
+        try
+        {
+            //New Kasa Entry
+            //--------------------------------------------------
+            //
+            // This only calculates the subtotals that will be 
+            // used in graph chart
+            //
+            //--------------------------------------------------
+            sso_APIResponse Rsp = new sso_APIResponse();
+
+            EntityManager em = DBPool.getSessionConnection(psUser_SessionInfo, Util.Methods.hash());
+
+            String sDate = psYear + "-" + psMonth + "-" + psDay;
+            boolean rc = bb.app.dekonts.DekontMisc.isEODAdded(em, "-1", sDate);
+            SsMrcDataEod cashTxn = new SsMrcDataEod();
+            if (rc==false)
+            {
+                //New Insert
+                cashTxn.stat        = 1;
+                cashTxn.mrcId       = "1";
+                cashTxn.txnAmount   = psTot;
+
+                cashTxn.txnDesc     = "";
+                cashTxn.txnDate     = sDate;
+                cashTxn.txnMonthNo  = psMonth;
+                cashTxn.txnType     = "KASA";
+                cashTxn.txnAmount   = psTot;
+
+                long lUID = em.persist(cashTxn);
+            }
+            else
+            {
+                rc = bb.app.dekonts.DekontMisc.updateEOD(em, "-1", sDate, psTot);
+            }
+            
+            //Rsp.Content = Util.JSON.Convert2JSON(summary).toString();
+            Rsp.Response = "ok";
+            return Rsp;
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+    }
+    
+    @GET
+    @Path("/api/getreportcell/{userid},"
+                                        + "{lang},"
+                                        + "{country},"
+                                        + "{sessionid}"
+                                        + "{mrcid},"
+                                        + "{bnkcode}"
+                                        + "{year}"
+                                        + "{monthno}"
+         )
+    @Consumes()
+    @Produces(MediaType.JSON)
+    public sso_APIResponse getReportByCell( @PathParam("userid")                       String psUser_SessionInfo,
+                                            @PathParam("lang")                         String psLang,
+                                            @PathParam("country")                      String psCountry,
+                                            @PathParam("sessionid")                    String psSessionId,
+                                            @PathParam("mrcid")                        String psMrcId,
+                                            @PathParam("bnkcode")                      String psBankCode,
+                                            @PathParam("year")                         String psYear,
+                                            @PathParam("monthno")                      String psMonthNo
+                                            ) throws Exception
+    {
+        int iRec = 0;
+        try
+        {
+            //--------------------------------------------------
+            //
+            // This only calculates the subtotals that will be 
+            // used in graph chart
+            //
+            //--------------------------------------------------
+            sso_APIResponse Rsp = new sso_APIResponse();
+
+            EntityManager em = DBPool.getSessionConnection(psUser_SessionInfo, Util.Methods.hash());
+
+            DekontSummary summary = new DekontSummary();
+
+            //int iBankCode = Integer.parseInt(psBankCode);
+            //int iYear     = Integer.parseInt(psYear);
+            //int iMonth    = Integer.parseInt(psMonthNo);
+            //summary = bb.app.dekonts.DekontMisc.calculateSummary(em,iBankCode, iYear, iMonth);
+            summary.banks = bb.app.dekonts.DekontMisc.calculateSummaryBankSubtotals(em, psYear, -1);
+
+            //long lUID = em.persist(newReport);
+            Rsp.Content = Util.JSON.Convert2JSON(summary).toString();
+            Rsp.Response = "ok";
+            return Rsp;
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+    }
+    
+    @GET
+    @Path("/api/getreportbank/{userid},"
+                                        + "{lang},"
+                                        + "{country},"
+                                        + "{sessionid}"
+                                        + "{mrcid},"
+                                        + "{bnkcode}"
+                                        //+ "{year}"
+                                        //+ "{monthno}"
+         )
+    @Consumes()
+    @Produces(MediaType.JSON)
+    public sso_APIResponse getReportByBank(@PathParam("userid")                       String psUser_SessionInfo,
+                                            @PathParam("lang")                         String psLang,
+                                            @PathParam("country")                      String psCountry,
+                                            @PathParam("sessionid")                    String psSessionId,
+                                            @PathParam("mrcid")                        String psMrcId,
+                                            @PathParam("bnkcode")                      String psBankCode) throws Exception
+    {
+        int iRec = 0;
+        try
+        {
+            sso_APIResponse Rsp = new sso_APIResponse();
+
+            EntityManager em = DBPool.getSessionConnection(psUser_SessionInfo, Util.Methods.hash());
+
+            DekontSummary summary = new DekontSummary();
+
+            int iBankCode = Integer.parseInt(psBankCode);
+            //int iYear     = Integer.parseInt(psYear);
+            //int iMonth    = Integer.parseInt(psMonthNo);
+            summary = bb.app.dekonts.DekontMisc.calculateSummary(em,iBankCode, -1, -1);
+
+            //long lUID = em.persist(newReport);
+            Rsp.Content = Util.JSON.Convert2JSON(summary).toString();
+            Rsp.Response = "ok";
+            return Rsp;
+
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+    }
+    
+    
     //Sample: http://localhost:8080/bb-wapi-dekont-converter/rest/bulbuller/dekont/api/showreport/?userid=1&lang=en&country=tr&sessionid=123&mrcid=1&repid=2&filename=abc.pdf
     
     @GET
-    @Path("/api/showreport/{userid},"
+    @Path("/api/processfile/{userid},"
                             + "{lang},"
                             + "{country},"
                             + "{sessionid}"
@@ -43,7 +290,7 @@ public class UI
          )
     @Consumes()
     @Produces(MediaType.JSON)
-    public sso_APIResponse showReport(  @PathParam("userid")                       String psUser_SessionInfo,
+    public sso_APIResponse processfile(  @PathParam("userid")                       String psUser_SessionInfo,
                                         @PathParam("lang")                         String psLang,
                                         @PathParam("country")                      String psCountry,
                                         @PathParam("sessionid")                    String psSessionId,
@@ -52,55 +299,90 @@ public class UI
                                         @PathParam("filename")                     String psFileName
                                      ) throws Exception
     {
-        sso_APIResponse Rsp = new sso_APIResponse();
-        
-        EntityManager em = DBPool.getSessionConnection(psUser_SessionInfo, Util.Methods.hash());
-        
-        //String sInFilePath  = "/Users/esabil/Documents/files/KUVEYT_Musterino_6667543_Ekno_3900_201991015217_ekstre.pdf";
-        //String sOutFilePath = "/Users/esabil/Documents/files/web_dekont_summary.txt";//output file
-        String sInFilePath  = "C:\\NEOTEMP\\CODE\\SHIPSHUK\\files\\Musterino_6667543_Ekno_101_201993154652_ekstre.pdf";
-        String sOutFilePath = "C:/NEOTEMP/CODE/SHIPSHUK/files/web_dekont_summary.txt";
-        ArrayList<DekontFields> Records = DekontMethods.processFile(sInFilePath, sOutFilePath, true);
 
-        SsMrcPosReports newReport = new SsMrcPosReports();
-        for(DekontFields recordN: Records)
+        int iRec = 0;
+        try
         {
-            newReport.mrcId      = psMrcId;
-            newReport.reportId   = psReportId;
-            newReport.fileName   = psFileName;
-            
-            newReport.txnAmount  = recordN.Amount;
-            newReport.txnBalance = recordN.Balance;
-            newReport.txnDate    = recordN.Date;
-            newReport.txnDesc    = recordN.Desc;
-            newReport.txnMonthNo = recordN.MonthNo;
-            newReport.txnReleaseDate = recordN.Release;
-            newReport.txnTime    = recordN.Time;
-            newReport.txnTraceNo = recordN.TraceNo;
-            newReport.txnType    = recordN.TxnType;
+            sso_APIResponse Rsp = new sso_APIResponse();
 
-            long lUID = em.persist(newReport);
+            EntityManager em = DBPool.getSessionConnection(psUser_SessionInfo, Util.Methods.hash());
+            
+            String sUserId = "123466";//for now
+            String sFilePath = DekontMisc.getFilePathbyId(psFileName, "/Users/esabil/Documents/uploads", sUserId);
+            
+            //String sInFilePath  = "/Users/esabil/Documents/files/KUVEYT_Musterino_6667543_Ekno_1_2019910152032_ekstre.pdf";
+            String sInFilePath  = sFilePath;
+            //String sInFilePath  = "/Users/esabil/Documents/files/KUVEYT_Musterino_6667543_Ekno_1_2019910152032_ekstre.pdf";
+            String sOutFilePath = "/Users/esabil/Documents/files/web_dekont_summary.txt";//output file
+
+
+        // pass the path to the file as a parameter 
+            //FileReader fr = new FileReader(sInFilePath); 
+
+            //int i; 
+            //while ((i=fr.read()) != -1) 
+              //System.out.print((char) i); 
+
+            //File x = new File(sInFilePath);
+            PDDocument document = null;
+            document = PDDocument.load(new File(sInFilePath));
+            //document = PDDocument.load(new File(sInFilePath));
+            document.getClass();
+
+            ArrayList<DekontFields> Records = DekontMethods.processFile(sInFilePath, sOutFilePath, true, "NECATİ BÜLBÜL");
+
+            SsMrcDataPosTxn newPosTxn = new SsMrcDataPosTxn();
+            for(DekontFields recordN: Records)
+            {
+                iRec++;
+                
+                if (iRec==7)
+                    iRec = iRec;
+                
+                newPosTxn.stat       = 1;
+                newPosTxn.mrcId      = psMrcId;
+                newPosTxn.reportId   = psReportId;
+                newPosTxn.fileName   = psFileName;
+                newPosTxn.bankCode   = Integer.parseInt(recordN.bankCode);
+                newPosTxn.txnAmount  = recordN.Amount.replace(",", "");//this is needed because txn_amount_num col calculated without comma
+                newPosTxn.txnBalance = recordN.Balance;
+                newPosTxn.txnDate    = recordN.Date;
+                newPosTxn.txnDesc    = recordN.Desc;
+                newPosTxn.txnMonthNo = recordN.MonthNo;
+                newPosTxn.txnReleaseDate = recordN.Release;
+                newPosTxn.txnTime    = recordN.Time;
+                newPosTxn.txnTraceNo = recordN.TraceNo;
+                newPosTxn.txnType    = recordN.TxnType;
+
+                //Check if the record exist
+                boolean rc = DekontMisc.isRecordAdded(em, newPosTxn.txnTraceNo, newPosTxn.bankCode );
+
+                if (rc==false)
+                {
+                    long lUID = em.persist(newPosTxn);
+                }
+                
+            }
+
+            //newReport.txnAmount = "1.10";
+            DekontSummary summary = new DekontSummary();
+
+            summary = bb.app.dekonts.DekontMisc.calculateSummary(em, -1, -1, -1);//-1 all in
+
+            //long lUID = em.persist(newReport);
+            Rsp.Content = Util.JSON.Convert2JSON(summary).toString();
+            Rsp.Response = "ok";
+            
+            return Rsp;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+            throw e;
         }
 
-        //newReport.txnAmount = "1.10";
-        DekontSummary summary         = new DekontSummary();
-        MonthSummary NewMonthSummary  = new MonthSummary();
-        MonthSummary TotMonthSummary  = new MonthSummary();
-        NewMonthSummary.MonthNo        = 1;//jan
-        NewMonthSummary.TotAll         = "120";
-        NewMonthSummary.TotInstallment = "50";
-        NewMonthSummary.TotUpfront     = "70";
-        summary.LastReportName = "Dosya1";
-        summary.MonthsLastReport.add(NewMonthSummary);
-        summary.MonthsAllReports.add(TotMonthSummary);
-        //long lUID = em.persist(newReport);
-        //Rsp.Content = 
-        Rsp.Content  = Util.JSON.Convert2JSON(summary).toString();
-        Rsp.Response = "ok";
-
-        return Rsp;
     }
-    
+
 }
 
 
